b84baca765b5da4d2693c486e6af8018eb451fe2
Anshu <jeeanshu24@gmail.com>
2024-09-21 10:15:00 +0530

    Commit 1

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "aws" {
+    region = "ap-south-1"
+}
+
+resource "aws_instance" "Test_module" {
+
+    ami = var.ami_value
+    instance_type = var.instance_type
+    subnet_id = var.subnet_id_value  
+    key_name = var.key_name_value
+}
