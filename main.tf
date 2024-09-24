9270e091db892ab6eb10bb2f4b4b45f827b70917
Anshu <jeeanshu24@gmail.com>
2024-09-24 10:39:41 +0530

    add key name

--- main.tf
+++ main.tf
@@ -1,4 +1,4 @@
-#Main file for providing backend of state file on S3 Bucket 
+# Main file for providing backend of state file on S3 Bucket 
 
 provider "aws" {
     region = "ap-south-1"
@@ -8,6 +8,7 @@ resource "aws_instance" "anshu" {
     ami = "ami-0522ab6e1ddcc7055"
     instance_type = "t2.micro"
     subnet_id = "subnet-079be55a8cf563fe9"
+    key_name = "key_1_pair"
  
 tags = {
         Name = "Anshu_TF_State_file"
